01074287aca1a29d7c60c9af5b4ba0e16e81999d
TorresMariana <mt.grafico@gmail.com>
2025-03-28 11:52:49 -0300

    menu hamburguesa

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function menuHamburguesa(){
+    document.getElementById('menu-hamburguesa').click();
+    return;
+}
+
+function cerrarMenuHamburguesa() {
+    document.getElementById('menu-hamburguesa').checked = false; // Desmarca el checkbox
+}
+
+
+
+
+
